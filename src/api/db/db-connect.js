c7dd5428904857468832e7491d3010872dacbbb3
applestopearss <timratsko909@gmail.com>
2024-03-04 16:46:28 -0800

    ADded a basic route

--- src/api/db/db-connect.js
+++ src/api/db/db-connect.js
@@ -1,68 +1,10 @@
-// const mongoose = require("mongoose");
-// require("dotenv").config();
-
-// async function dbConnect() {
-//     mongoose.connect(process.env.DATABASE_URL, 
-//     {
-//         useUnifiedTopology: true,
-//         useNewUrlParser: true
-//     })
-//     .then(() => console.log("MongoDB connected"))
-//     .catch((error) => console.log(`error: ${error}`));
-// }
-
-// module.exports = dbConnect;
-
-
-
 require('dotenv').config()
 
 let db = process.env.DATABASE_URL;
-
-console.log("this is " + db);
-
-
-
-const express = require('express');
 const mysql = require('mysql');
-
-
-// const mysqlConfig = {
-//     host: "aws.connect.psdb.cloud", 
-//     port: 3306,
-//     user: "gogvaiwd7d188g976xvq", 
-//     password: "",
-//     database: "husky-huddle",
-//     debug: false // Connection debugging mode is ON
-// };
-
-
-// const dbConnection = mysql.createConnection(mysqlConfig);
-
-// console.log('Backend is now connected to: ' + dbConnection.config.database + '.');
-
-// dbConnection.query(sqlQuery), (err, result) => {
-//     if (err) {
-//         // console.log("Error");
-//     } 
-//     // console.log("result");
-//     // console.log("Something here" + result);
-//     // console.log("result2");
-// };
 
 const connection = mysql.createConnection(db);
 
-// const app = express();
-
-var result;
-var result1 = "test";
-
-//connection = mysql.createConnection(DATABASE_URL);
-
-async function dbConnect() {
-    console.log("mysql is connected to " + connection.config.database);
-
-    return connection;
-}
+console.log("Connected to " + connection.config.database);
 
 module.exports = connection;
--- src/api/routes/guild.js
+++ src/api/routes/guild.js
@@ -4,28 +4,74 @@ var router = express.Router();
 
 const databaseConnect = require("../db/db-connect");
 
-let dbConnection = databaseConnect;
+// let dbConnection = databaseConnect;
 
 router.get("/", function(req, res, next) {
 
+    const sqlQuery = "SELECT * FROM Guild;"
+    databaseConnect.query(sqlQuery, (err, result) => {
+        if (err) {
+            console.log("Error");
+            res.status(400);
+        } 
+        // console.log("result");
+        // console.log(result);
+        // console.log("result2");
+        // result1 = result;
+        return res.status(200).json(result);
+    });
 
-    res.send("Guild API is working properly");
+
+});
+
+router.get("/user/", function(req, res, next) {
+
+    // res.send("Guild API is working properly");
 
     console.log(databaseConnect.config.database);
-    const sqlQuery = "SELECT * FROM categories;"
+    const sqlQuery = 
+        `SELECT User.*, GuildUser.Role, Guild.GuildName
+        FROM User
+        INNER JOIN GuildUser ON User.UserID = GuildUser.UserID
+        INNER JOIN Guild ON GuildUser.GuildID = Guild.GuildID
+        ORDER BY Guild.GuildName, User.UserName;`
 
 
     databaseConnect.query(sqlQuery, (err, result) => {
         if (err) {
             console.log("Error");
         } 
-        console.log("result");
         console.log(result);
-        console.log("result2");
-        result1 = result;
+
+
+        return res.status(200).json(result);
     });
+});
 
 
+router.get("/user/user/", function(req, res, next) {
+
+    // res.send("Guild API is working properly");
+
+    console.log(databaseConnect.config.database);
+    const sqlQuery = 
+        `SELECT User.*, GuildUser.Role, Guild.GuildName
+        FROM User
+        INNER JOIN GuildUser ON User.UserID = GuildUser.UserID
+        INNER JOIN Guild ON GuildUser.GuildID = Guild.GuildID
+        ORDER BY Guild.GuildName, User.UserName;`
+
+
+    databaseConnect.query(sqlQuery, (err, result) => {
+        if (err) {
+            console.log("Error");
+        } 
+        console.log(result);
+
+        res.setHeader('user', "USERNAME");
+        res.setHeader('pass', "PASSWORD");
+        return res.status(200).json(result);
+    });
 });
 
 module.exports = router;
