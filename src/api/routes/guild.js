bc3615667d7037f4af32b0f9134eaf4a416ca523
applestopearss <timratsko909@gmail.com>
2024-03-07 02:36:10 -0800

    Got post, put and delete work for the guild webservice.

--- src/api/routes/guild.js
+++ src/api/routes/guild.js
@@ -6,6 +6,7 @@ const databaseConnect = require("../db/db-connect");
 
 // let dbConnection = databaseConnect;
 
+// Get all guilds, returns a json with all guilds 
 router.get("/", function(req, res, next) {
 
     console.log("Guild API");
@@ -17,14 +18,111 @@ router.get("/", function(req, res, next) {
             console.log(err);
             res.status(400);
         } 
-        // console.log("result");
-        // console.log(result);
-        // console.log("result2");
-        // result1 = result;
         return res.status(200).json(result);
     });
+});
 
+// Create a new guild with the name provided in the body of the request with GuildName : "Some guild name goes here" in the json
+// {"GuildName" : "newGuild"} as an example as of what to put in the body
+router.post("/", function(req, res, next) {
 
+    console.log("Guild POST API");
+
+    let GuildName = req.body.GuildName;
+    console.log(GuildName);
+
+    const sqlQuery = 
+        `INSERT INTO Guild (GuildName)
+        VALUES ('${GuildName}');`;
+    databaseConnect.query(sqlQuery, (err, result) => {
+        if (err) {
+            console.log("Error");
+            console.log(err);
+            res.status(400);
+        } 
+        return res.status(200).json(result);
+    });
+});
+
+// Update a guild with a new name based off of the guildID
+router.put("/", function(req, res, next) {
+
+    console.log("Guild update");
+
+    let GuildName = req.body.GuildName;
+    let GuildID = req.body.GuildID;
+
+    const sqlQuery = 
+        `UPDATE Guild
+        SET GuildName = '${GuildName}'
+        WHERE GuildID = ${GuildID};`;
+    databaseConnect.query(sqlQuery, (err, result) => {
+        if (err) {
+            console.log("Error");
+            console.log(err);
+            res.status(400);
+        } 
+        return res.status(200).json(result);
+    });
+});
+
+// Delete a guild and also remove all members of that guild by deleting all guildUsers of that guild
+router.delete("/", function(req, res, next) {
+
+    console.log("Guild Delete");
+    let GuildID = req.body.GuildID;
+
+    // Delete the members of a guild, this is safe because the users are still guildusers
+    // of any other guild they are a part of but they are removed from this specific guild  
+    const sqlQuery = 
+        `DELETE FROM GuildUser
+        WHERE GuildID = ${GuildID};`
+    const sqlQuery2 =     
+        `DELETE FROM Guild
+        WHERE GuildID = ${GuildID};`
+    databaseConnect.query(sqlQuery, (err, result) => {
+        if (err) {
+            console.log("Error");
+            console.log(err);
+            res.status(400);
+        } 
+        //return res.status(200).json(result);
+    });
+    databaseConnect.query(sqlQuery2, (err, result) => {
+        if (err) {
+            console.log("Error");
+            console.log(err);
+            res.status(400);
+        } 
+        return res.status(200).json(result);
+    });
+});
+
+router.get("/user/", function(req, res, next) {
+
+    console.log("Guilds of a specific user");
+
+    let username = req.headers.username;
+    let password = req.headers.password;
+
+    const sqlQuery = 
+        `SELECT g.GuildID, g.GuildName
+        FROM GuildUser gu
+        JOIN Guild g ON gu.GuildID = g.GuildID
+        WHERE gu.UserID = (
+            SELECT UserID
+            FROM User
+            WHERE UserName = '${username}' AND UserPass = '${password}'
+        );`
+
+    databaseConnect.query(sqlQuery, (err, result) => {
+        if (err) {
+            console.log("Error");
+            console.log(err);
+            res.status(400);
+        } 
+        return res.status(200).json(result);
+    });
 });
 
 router.get("/user/", function(req, res, next) {
@@ -56,6 +154,12 @@ router.get("/user/user/", function(req, res, next) {
 
     // res.send("Guild API is working properly");
 
+
+
+    let username = req.headers.username;
+    let password = req.headers.password;
+
+
     console.log(databaseConnect.config.database);
     const sqlQuery = 
         `SELECT User.*, GuildUser.Role, Guild.GuildName
@@ -69,7 +173,7 @@ router.get("/user/user/", function(req, res, next) {
         if (err) {
             console.log("Error");
         } 
-        console.log(result);
+        //console.log(result);
 
         res.setHeader('user', "USERNAME");
         res.setHeader('pass', "PASSWORD");
