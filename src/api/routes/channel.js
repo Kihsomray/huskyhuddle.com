532c19ea3d7879385c6ec23963e832c2827b728a
Louis Lomboy <124099680+HrothgarFF14@users.noreply.github.com>
2024-03-08 15:31:15 -0800

    finished /channel/message endpoint

--- src/api/routes/channel.js
+++ src/api/routes/channel.js
@@ -1,58 +1,121 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 const databaseConnect = require("../db/db-connect");
 
-router.get("/", function(req, res, next) {
-    res.send("Channel API is working properly");
-});
+router.get("/", function (req, res, next) {
+    console.log("Getting all Channel ID and Names...");
 
-//Get all the channels by guild id
-router.get("/channels", function (req, res, next) {
-    
-    const guildId = req.query.guildId;
+    const sqlQuery = `SELECT *  FROM Channel;`;
 
-    if (!guildId) {
-        return res.status(400).json({error:"No GuildId was specified"});
-    }
-
-    const sqlQuery = "SELECT * FROM channels WHERE GuildID = ?;";
-
-    databaseConnect.query(sqlQuery, (err, channels) => {
+    databaseConnect.query(sqlQuery, (err, result) => {
         if (err) {
-            console.error("Error fetching channles using GuildID");
+            console.log("Error");
             res.status(400);
         }
-
-        return res.status(200).json(channels);
+        return res.status(200).json(result);
     });
 });
 
-//get specific channel within a guild
-router.get("/channels/:channelId", function(req, res) {
-    const channelId = req.params.channelId;
+// Get the latest x messages sent into the channel
+// {"Limit" : "5", ChannelID: "1"} as an example as of what to put in the body
+router.get("/message/", function (req, res) {
+    let limit = req.body.Limit;
+    let channelID = req.body.ChannelId;
 
-    const sqlQuery = "SELECT * FROM channels where ChannelId = ?;"
+    console.log(`Getting the last ${limit} messages sent into the channel`);
 
-    databaseConnect.query(sqlQuery, [channelId], (err, channel) => {
+    const sqlQuery = `SELECT * FROM Message 
+                    WHERE ChannelID = ${channelID};`;
+
+    databaseConnect.query(sqlQuery, (err, res) => {
         if (err) {
-            console.error("Error fetching channel using ChannelID")
+            console.log(
+                "Error getting the last ${limit} messages sent into the channel"
+            );
+            res.statis(400);
+        }
+        return res.status(200).json(res);
+    });
+});
+
+//POST request to handler to add a message to a channel
+
+router.post("/message/", function (req, res) {
+    let channelId = res.body.ChannelId;
+    let messageContent = res.body.Content;
+
+    console.log("Sending a message to channel");
+
+    if (!ChannelId || !messageContent) {
+        return res
+            .status(400)
+            .json({ error: "ChannelID, MessageContent are required in body." });
+    }
+
+    const sqlQuery = `INSERT INTO Message (ChannelID, MessageContent) 
+                    VALUES (${channelId}, ${messageContent});`;
+
+    databaseConnect.query(sqlQuery, (err, res) => {
+        if (err) {
+            console.log("Error adding message to channel", err);
             res.status(400);
         }
+        return res.status(200).json(res);
+    });
+});
 
-        if (!channel || channel.length === 0) {
-            return res.status(404).json({error: "No channel found using ChannelID"});
+//Edit message in a specific channel
+router.put("/message/", function (req, res) {
+    let channelId = req.body.ChannelId;
+    let messageId = req.body.MessageId;
+    let messageContent = req.body.MessageContent;
+
+    if (!channelId || !messageId || !messageContent) {
+        return res.status(400).json({
+            error: "ChannelId, MessageId, and MessageContent are required.",
+        });
+    }
+
+    const sqlQuery = `UPDATE Message SET MessageContent = ${messageContent} 
+                    WHERE ChannelID = ${channelId} AND MessageID = ${messageId};`;
+
+    databaseConnect.query(sqlQuery, (err, result) => {
+        if (err) {
+            console.log("Error updating message content", err);
+            res.status(400);
         }
+        return res.status(200).json(result);
+    });
+});
 
-        return res.status(200).json(channel);
-    })
-})
+// Delete a message from this Channel
+router.delete("/message/", function (req, res) {
+    let channelId = req.body.ChannelId;
+    let messageId = req.body.MessageId;
+
+    if (!channelId || !messageId) {
+        return res
+            .status(400)
+            .json({ error: "ChannelId and messageId are required in body" });
+    }
+
+    let sqlQuery = `DELETE FROM Message
+                    WHERE ChannelID = ${channelId} AND MessageID = ${messageId};`;
+
+    databaseConnect.query(sqlQuery, (err, result) => {
+        if (err) {
+            console.log("Error deleting message", err);
+            res.status(400);
+        }
+        return res.status(200).json(result);
+    });
+});
 
 //delete a spcific channel using channelId
 
 //patch. Changes the channel name within guild
 
 //Post. Create a new channel within a guild
-
 
 module.exports = router;
