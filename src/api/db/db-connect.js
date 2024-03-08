9e336904d9295e501b8cfe5137dbc51211d5ef80
applestopearss <timratsko909@gmail.com>
2024-03-07 21:33:38 -0800

    Change the username

--- src/api/db/db-connect.js
+++ src/api/db/db-connect.js
@@ -7,7 +7,7 @@ const mysql = require('mysql');
 const mysqlConfig = {
     host: "aws.connect.psdb.cloud", 
     port: 3306,
-    user: "gogvaiwd7d188g976xvq", 
+    user: "op70lagh4h23piezyd99", 
     password: "", // REMOVE THIS IF YOU PUSH AT ANYTIME
     database: "husky-huddle",
     ssl: true,
