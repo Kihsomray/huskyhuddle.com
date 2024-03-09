7103ec334f25efa3b58792bcabfaa1a0861f410d
Louis Lomboy <124099680+HrothgarFF14@users.noreply.github.com>
2024-03-08 19:24:52 -0800

    tested channel.js and fixed bugs

--- src/api/routes/guild.js
+++ src/api/routes/guild.js
@@ -15,7 +15,9 @@ router.get("/", function (req, res, next) {
     databaseConnect.query(sqlQuery, (err, result) => {
         if (err) {
             console.log("Error");
-            return res.status(400);
+            return res
+                .status(400)
+                .json({ error: "Couldn't connect to database" });
         }
         return res.status(200).json(result);
     });
@@ -34,9 +36,9 @@ router.post("/", function (req, res, next) {
         if (err) {
             console.log("Error");
             console.log(err);
-            return res.status(400);
+            return result.status(400);
         }
-        return res.status(200).json(result);
+        return result.status(200).json(result);
     });
 });
 
