c0c06ff1a5b6c8fe5b04ab0b2104c951427b09f8
Louis Lomboy <124099680+HrothgarFF14@users.noreply.github.com>
2024-03-13 00:12:32 -0700

    finished /random/user/guild endpoint

--- src/api/routes/random.js
+++ src/api/routes/random.js
@@ -81,4 +81,51 @@ router.post("/user/", async function (req, res, next) {
     }
 });
 
+//Creates a random user from an external API and puts them into a guild
+//{ "GuildID" = X, "GuildRole" = X} is the reqeust body format
+router.post("/user/guild/", async function (req, res, next) {
+    console.log(
+        "Creating a random user to be added into the database and into a guild..."
+    );
+    let GuildId = req.body.GuildID;
+    let GuildRole = req.body.GuildRole;
+
+    try {
+        //Fetch random user data from the randomuser.me api
+        let response = await axios.get("https://randomuser.me/api/");
+        let userData = response.data.results[0];
+
+        //Extract the randm user information
+        let userName = `${userData.login.username}`;
+        let userEmail = userData.email;
+        //let userPassword = userData.login.password;
+
+        let sqlQuery = `INSERT INTO User (UserName, UserEmail, UserPass)
+            VALUES ("${userName}","${userEmail}","password");`;
+        databaseConnect.query(sqlQuery, (err, result) => {
+            if (err) {
+                return res.status(400).json({
+                    "Error executing SQL statement to create a random user in the database":
+                        err,
+                });
+            }
+            const userID = result.insertId;
+
+            const nextQuery = `INSERT INTO GuildUser (GuildID, UserID, Role)
+                                VALUES (${GuildId},${userID},"${GuildRole}");`;
+
+            databaseConnect.query(nextQuery, (err, result) => {
+                if (err) {
+                    return res.status(400).json({
+                        "Error executing SQL statement to put newly created user in GuildUser database":
+                            err,
+                    });
+                }
+                return res.status(200).json(result);
+            });
+        });
+    } catch (err) {
+        res.status(500).json({ "Error gathering data from external api": err });
+    }
+});
 module.exports = router;
