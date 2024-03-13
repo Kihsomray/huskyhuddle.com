ce7c11dec197cd0c3be33a92a5579d698ee0ec96
applestopearss <timratsko909@gmail.com>
2024-03-12 18:11:57 -0700

    Admin update channel and delete channel endpoints are up and running

--- src/api/routes/admin.js
+++ src/api/routes/admin.js
@@ -7,7 +7,7 @@ const databaseConnect = require("../db/db-connect");
 
 //// Web service for all admin commands
 
-// Get all guilds, returns a json with all guilds
+// Check if this user is an admin
 router.get("/", async function (req, res, next) {
     console.log("Admin API");
 
@@ -79,11 +79,110 @@ router.post("/channel/", function (req, res, next) {
             if (err) {
                 console.log("Error");
                 console.log(err);
-                return result.status(400).json({"Error" : "Uhoh"});
+                return result.status(400).json({"Error" : err});
             }
             return res.status(200).json({"ChannelID" : result.insertId});
         });
     });
 });
+
+// change a channels name within a guild with the name provided.
+// The userid, guildid and channelname, channelid is to be passed in the header.
+router.put("/channel/", function (req, res, next) {
+    console.log("/Admin/channel/ POST API");
+
+    const UserID = req.headers.userid;
+    const GuildID = req.headers.guildid;
+    const ChannelID = req.headers.channelid;
+    const ChannelName = req.headers.channelname;
+
+    const sqlQueryRole = 
+        `SELECT Role
+        FROM GuildUser
+        WHERE UserID = ${UserID} AND GuildID = ${GuildID};`;
+
+    databaseConnect.query(sqlQueryRole, (err, result) => {
+        if (err) {
+            console.log("Error");
+            console.log(err);
+            return result.status(400).json({"Error" : "Uhoh"});
+        }
+
+        console.log(result);
+        console.log(result.length);
+
+        if (result.length == 0) {
+            return res.status(400).json({"Error" : "User is not member of Guild"});
+        } else if (result[0].Role !== "Admin") {
+            return res.status(400).json({"Error" : "User is not an Admin"});
+        }
+        
+        //return res.status(200).json(result[0]);
+        // If here the user is an admin
+
+        const sqlQuery = 
+            `UPDATE Channel
+            SET ChannelName = '${ChannelName}'
+            WHERE ChannelID = ${ChannelID} AND GuildID = ${GuildID};`;
+        databaseConnect.query(sqlQuery, (err, result) => {
+            if (err) {
+                console.log("Error");
+                console.log(err);
+                return result.status(400).json({"Error" : err});
+            }
+            return res.status(200).json({"ChannelID" : result.insertId});
+        });
+    });
+});
+
+// Delete a channel within a guild with the channelid provided.
+// The userid, guildid and channelid is to be passed in the header.
+router.delete("/channel/", function (req, res, next) {
+    console.log("/Admin/channel/ DELETE API");
+
+    const UserID = req.headers.userid;
+    const GuildID = req.headers.guildid;
+    const ChannelID = req.headers.channelid;
+
+    const sqlQueryRole = 
+        `SELECT Role
+        FROM GuildUser
+        WHERE UserID = ${UserID} AND GuildID = ${GuildID};`;
+
+    databaseConnect.query(sqlQueryRole, (err, result) => {
+        if (err) {
+            console.log("Error");
+            console.log(err);
+            return result.status(400).json({"Error" : "Uhoh"});
+        }
+
+        console.log(result);
+        console.log(result.length);
+
+        if (result.length == 0) {
+            return res.status(400).json({"Error" : "User is not member of Guild"});
+        } else if (result[0].Role !== "Admin") {
+            return res.status(400).json({"Error" : "User is not an Admin"});
+        }
+        
+        //return res.status(200).json(result[0]);
+        // If here the user is an admin
+
+        const sqlQuery = 
+            `DELETE FROM Channel
+            WHERE ChannelID = ${ChannelID} AND GuildID = ${GuildID};`;
+        databaseConnect.query(sqlQuery, (err, result) => {
+            if (err) {
+                console.log("Error");
+                console.log(err);
+                return result.status(400).json({"Error" : err});
+            }
+            return res.status(200).json({"NumChannelsRemoved": result.affectedRows});
+        });
+    });
+});
+
+
+
 
 module.exports = router;
