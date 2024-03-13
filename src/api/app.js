f28e2ffe652362a219f1515cdc497c44425bd159
Louis Lomboy <124099680+HrothgarFF14@users.noreply.github.com>
2024-03-13 04:06:43 -0700

    created joke.js

--- src/api/app.js
+++ src/api/app.js
@@ -1,9 +1,9 @@
-var createError = require('http-errors');
-var express = require('express');
-const cors = require('cors');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+const cors = require("cors");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
 // const databaseConnect = require("./db/db-connect");
 
@@ -11,62 +11,57 @@ var app = express();
 
 app.use(cors());
 
-
-
 // connect to database
 // let dbConnection = databaseConnect();
 
 // console.log(dbConnection.config.database);
 
-
-
 // const sqlQuery = "SELECT * FROM categories;"
 // databaseConnect.query(sqlQuery, (err, result) => {
 //   if (err) {
 //       console.log("Error");
-//   } 
+//   }
 //   console.log("result");
 //   console.log(result);
 //   console.log("result2");
 //   result1 = result;
 // });
 
-
-
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', require('./routes/info'));
-app.use('/channel', require('./routes/channel'));
-app.use('/dm', require('./routes/direct-message'));
-app.use('/guild', require('./routes/guild'));
-app.use('/message', require('./routes/message'));
-app.use('/role', require('./routes/role'));
-app.use('/user', require('./routes/user'));
-app.use('/admin', require('./routes/admin'));
-app.use('/random', require('./routes/random'));
+app.use("/", require("./routes/info"));
+app.use("/channel", require("./routes/channel"));
+app.use("/dm", require("./routes/direct-message"));
+app.use("/guild", require("./routes/guild"));
+app.use("/message", require("./routes/message"));
+app.use("/role", require("./routes/role"));
+app.use("/user", require("./routes/user"));
+app.use("/admin", require("./routes/admin"));
+app.use("/random", require("./routes/random"));
+app.use("/joke", require("./routes/joke"));
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render("error");
 });
 
 module.exports = app;
---
+++ src/api/routes/joke.js
@@ -0,0 +1,58 @@
+var express = require("express");
+var router = express.Router();
+var axios = require("axios");
+
+const databaseConnect = require("../db/db-connect");
+
+/*
+ * @swagger
+ * /joke/message/
+ *   post:
+ *     summary: Create a new message with a random quote
+ *     description: Create a new message with a random quote by a user from an external API
+ *     responses:
+ *       200:
+ *         description: Successfully created message
+ *       400:
+ *         description: Error creating message
+ */
+
+router.post("/message/", async function (req, res, next) {
+    let ChannelId = req.body.ChannelID;
+    let GuildID = req.body.GuildID;
+    let UserID = req.body.UserID;
+
+    if (!ChannelId || !GuildID || !UserID) {
+        return res.status(400).json({
+            error: "ChannelID, GuildID, and UserID is required in the body in the request",
+        });
+    }
+    try {
+        const jokeResponse = await axios.get(
+            "https://official-joke-api.appspot.com/random_joke"
+        );
+        const jokeData = jokeResponse.data;
+
+        let messageContent = `${jokeData.setup}\n${jokeData.punchline}`;
+
+        const sqlQuery = `INSERT INTO Message (GuildID, ChannelID, UserID, MessageContent)
+                        VALUES (${GuildID}, ${ChannelId}, ${UserID}, "${messageContent}");`;
+
+        databaseConnect.query(sqlQuery, (err, result) => {
+            if (err) {
+                console.log(
+                    "Error reading SQL statement to create random message:"
+                );
+                return res.status(400).json(err);
+            }
+            return res.status(200).json(result);
+        });
+    } catch (error) {
+        console.error("Error creating random new message");
+        return res
+            .status(500)
+            .json({ error: "Error gathering data from external api" });
+    }
+});
+
+module.exports = router;
